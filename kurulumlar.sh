d6fca29dfb27b7690397e10a7ca3c465a62228b0
mftnakrsu <57320216+mftnakrsu@users.noreply.github.com>
2021-09-16 20:32:46 +0300

    kurulumlar.sh uploaded

---
+++ kurulumlar.sh
@@ -0,0 +1,21 @@
+
+sudo apt-get install ros-noetic-rosserial-*
+sudo apt-get install ros-noetic-cv-*
+sudo apt-get install ros-noetic-joy*
+
+sudo apt-get install ros-noetic-ackermann-*
+sudo apt-get install ros-noetic-control*
+sudo apt-get install ros-noetic-joint-*
+sudo apt-get install ros-noetic-effort-controllers
+sudo apt-get install ros-noetic-position-controllers
+sudo apt-get install ros-noetic-diff-drive-controller
+
+sudo apt-get install ros-noetic-gazebo-*
+
+sudo apt-get install ros-noetic-hector-*
+sudo apt-get install ros-noetic-gmapping
+sudo apt-get install ros-noetic-slam-*
+
+sudo apt-get install ros-noetic-turtlebot*
+sudo apt-get install ros-noetic-openni-*  
+sudo apt-get install ros-noetic-freenect-*
